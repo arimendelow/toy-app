022bca77d6893725db212199ce716d796f29ca0a
Ari Mendelow <ari@mendelow.me>
2019-06-22 20:21:49 -0500

    Validate the presence of micropost content (posts cannot be blank)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-
   # Constrain micropost length to 140 chars
-  validates :content, length: { maximum: 140 } 
+  validates :content, length: { maximum: 140 }, presence: true
+
 end
